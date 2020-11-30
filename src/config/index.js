9e154dfef20d6195bc5695d64259955c673064ae
wang <591674141>
2020-11-30 18:08:34 +0800

    恢复测试配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,7 @@
 export const service = {
   baseUrl: {
     // fallback: 'http://10.60.85.201/'
-    fallback: 'http://10.60.82.185/'
-    // fallback: 'http://10.60.17.43:8080/gdzcgl/'
+    fallback: 'http://10.60.17.43:8080/gdzcgl/'
   },
   getBaseUrl() {
     return this.baseUrl.fallback
